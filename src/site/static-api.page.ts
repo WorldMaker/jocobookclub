dd076fa3dc87ecddad8667a367f6015ac9138fe0
Max Battcher <me@worldmaker.net>
2024-07-29 22:11:50 -0400

    Add URL to static API

--- src/site/static-api.page.ts
+++ src/site/static-api.page.ts
@@ -1,33 +1,49 @@
 interface BookInfo {
-    title: string
-    author: string
-    scheduledDate?: Date
-    ltid: string
+  title: string
+  author: string
+  scheduledDate?: Date
+  ltid: string
+  url: string
 }
 
 interface BooksByLtid {
-    [key: string]: BookInfo
+  [key: string]: BookInfo
 }
 
 function byLtId(books: BookInfo[]): BooksByLtid {
-    return books.reduce((acc, book) => ({ ...acc, [book.ltid]: book }), {} as BooksByLtid)
+  return books.reduce(
+    (acc, book) => ({ ...acc, [book.ltid]: book }),
+    {} as BooksByLtid,
+  )
 }
 
-export default function *staticApi({ search }: Lume.Data) {
-    const previousBooks = search.pages('previous')
-        .map(page => ({ title: page.title!, author: page.author, scheduledDate: page.date, ltid: page.ltid }))
-    const previousBooksByLtid = byLtId(previousBooks)
-    yield {
-        url: '/static-api/previous.json',
-        contentType: "application/json",
-        content: JSON.stringify(previousBooksByLtid)
-    }
-    const upcomingBooks = search.pages('upcoming')
-        .map(page => ({ title: page.title!, author: page.author, scheduledDate: page.scheduled ? page.date : undefined, ltid: page.ltid }))
-    const upcomingBooksByLtid = byLtId(upcomingBooks)
-    yield {
-        url: '/static-api/upcoming.json',
-        contentType: "application/json",
-        content: JSON.stringify(upcomingBooksByLtid)
-    }
+export default function* staticApi({ search }: Lume.Data) {
+  const previousBooks = search.pages('previous')
+    .map((page) => ({
+      title: page.title!,
+      author: page.author,
+      scheduledDate: page.date,
+      ltid: page.ltid,
+      url: page.url,
+    }))
+  const previousBooksByLtid = byLtId(previousBooks)
+  yield {
+    url: '/static-api/previous.json',
+    contentType: 'application/json',
+    content: JSON.stringify(previousBooksByLtid),
+  }
+  const upcomingBooks = search.pages('upcoming')
+    .map((page) => ({
+      title: page.title!,
+      author: page.author,
+      scheduledDate: page.scheduled ? page.date : undefined,
+      ltid: page.ltid,
+      url: page.url,
+    }))
+  const upcomingBooksByLtid = byLtId(upcomingBooks)
+  yield {
+    url: '/static-api/upcoming.json',
+    contentType: 'application/json',
+    content: JSON.stringify(upcomingBooksByLtid),
+  }
 }
