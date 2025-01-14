5e0748bf639f93976373ee980b985dc2b446af13
Max Battcher <me@worldmaker.net>
2025-01-13 20:21:13 -0500

    Add a bucket tallied queue message

--- src/api/models/voting.ts
+++ src/api/models/voting.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod'
 import { UserId } from './user.ts'
+import { Bucket } from './tally.ts'
 
 export const QueueMessages = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('bucket-tallied'),
+    bucket: z.string(),
+    at: z.date(),
+  }),
   z.object({
     type: z.literal('user-voted'),
     userId: UserId,
@@ -10,6 +16,14 @@ export const QueueMessages = z.discriminatedUnion('type', [
 ])
 
 export type QueueMessages = z.infer<typeof QueueMessages>
+
+export function queueTallied(kv: Deno.Kv, bucket: Bucket) {
+  return kv.enqueue({
+    type: 'bucket-tallied',
+    bucket,
+    at: new Date(),
+  })
+}
 
 export function queueVoted(kv: Deno.Kv, userId: UserId) {
   return kv.enqueue({
