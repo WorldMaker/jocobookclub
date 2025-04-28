0fd11a1db438c065012b37e2a5caf38422743223
Max Battcher <me@worldmaker.net>
2025-04-28 18:55:10 -0400

    Add save-suggestions script

---
+++ scripts/save-suggestions.ts
@@ -0,0 +1,46 @@
+import { slugify } from 'jsr:@bossley9/slugify@1.0.1'
+import { stringify } from 'jsr:@std/yaml@1.0.5'
+import { listSuggestions } from '../src/api/models/suggestion.ts'
+
+// Save the suggestions to disk
+// You will need to configure environment variables DENO_KV_ACCESS_TOKEN
+// and CLUB_KV_URL
+
+const hasKvAccess = Deno.env.has('DENO_KV_ACCESS_TOKEN')
+const clubUrl = Deno.env.get('CLUB_KV_URL')
+
+if (!hasKvAccess || !clubUrl) {
+  console.log(
+    'You need to set the DENO_KV_ACCESS_TOKEN and CLUB_KV_URL environment variables',
+  )
+  Deno.exit(1)
+}
+
+const kv = await Deno.openKv(clubUrl)
+
+const suggestions = await listSuggestions(kv)
+
+for (const suggestion of suggestions) {
+  const slug = slugify(suggestion.title)
+  const filename = `src/site/held/${slug}.md`
+
+  const contents = `
+---
+${stringify(suggestion, { indent: 2 })}
+---
+
+Blurb from a JoCoNaut:
+
+> ${suggestion.whyBlurb}
+ 
+Content warnings include ${suggestion.cw}
+`
+
+  await Deno.writeTextFile(filename, contents, {
+    create: true,
+  })
+}
+
+const fmtcmd = new Deno.Command(Deno.execPath(), { args: ['fmt'] })
+const { code: fmtcode } = await fmtcmd.output()
+console.assert(fmtcode === 0, 'Error running deno fmt')
