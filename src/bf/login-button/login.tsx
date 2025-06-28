5402acb764854314219538a21a0ba4192fc0f161
Max Battcher <me@worldmaker.net>
2025-06-28 17:48:21 -0400

    🔧 deno fmt

--- src/bf/login-button/login.tsx
+++ src/bf/login-button/login.tsx
@@ -7,7 +7,12 @@ interface LoginProps {
 
 export default function Login({ url, active }: LoginProps) {
   return (
-    <a class={ active ? 'navbar-item icon-text has-background-dark has-text-info' : 'navbar-item icon-text' } href={url ?? '/login'}>
+    <a
+      class={active
+        ? 'navbar-item icon-text has-background-dark has-text-info'
+        : 'navbar-item icon-text'}
+      href={url ?? '/login'}
+    >
       <span class='icon'>
         <i class='fa-duotone fa-solid fa-ferry' />
       </span>
