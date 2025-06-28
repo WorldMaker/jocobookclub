17349dafa073d76b42e55e27eddeacf8577dd094
Max Battcher <me@worldmaker.net>
2025-06-28 18:25:10 -0400

    Add active styling to logged in user button

--- src/bf/login-button/user.tsx
+++ src/bf/login-button/user.tsx
@@ -6,15 +6,20 @@ export interface UserProps {
   email: Observable<string | null>
   session: Observable<Session | null>
   url: string | null
+  active: Observable<boolean>
 }
 
-export default function User({ email, session, url }: UserProps) {
+export default function User({ email, session, url, active }: UserProps) {
   const admin = session.pipe(map((session) => session?.admin ?? false))
   const notAdmin = admin.pipe(map((admin) => !admin))
   return (
     <a
       class='navbar-item icon-text'
       href={url ?? '/passkey'}
+      classBind={{ 
+        'has-text-info': active,
+        'has-background-dark': active,
+      }}
     >
       <span class='icon'>
         <i
