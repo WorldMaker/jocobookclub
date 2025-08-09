0ce7c526feda38cab0819eca827bf9b90492e399
Max Battcher <me@worldmaker.net>
2025-08-09 18:41:52 -0400

    Add invite creation to the Administration section

--- src/bf/admin-section/main.tsx
+++ src/bf/admin-section/main.tsx
@@ -6,23 +6,74 @@ import {
   ObservableEvent,
   run,
 } from '@worldmaker/butterfloat'
-import { AdminVm } from './vm.ts'
-import sessionManager from '../vm/session-manager.ts'
+import { Invite } from '@worldmaker/jocobookclub-api/models'
 import { map, Subscription } from 'rxjs'
+import sessionManager from '../vm/session-manager.ts'
+import { AdminVm } from './vm.ts'
 
 interface AdminProps {
   vm: AdminVm
 }
 
 interface AdminEvents {
+  createEmailInvite: ObservableEvent<SubmitEvent>
+  createOpenInvite: ObservableEvent<MouseEvent>
+  emailChanged: ObservableEvent<KeyboardEvent>
   recount: ObservableEvent<MouseEvent>
+}
+
+function InviteView({ invite }: { invite: Invite }) {
+  const inviteUrl =
+    `https://worldmaker.net/jocobookclub/invite-register/#${invite.id}`
+
+  if (invite.type === 'specific-email') {
+    return (
+      <p class='block'>
+        Created invite link:{' '}
+        <a href={inviteUrl}>
+          <code>{inviteUrl}</code>
+        </a>{' '}
+        for email:{' '}
+        <a href={`mailto:${invite.email}`}>
+          <code>{invite.email}</code>
+        </a>
+      </p>
+    )
+  }
+
+  return (
+    <p class='block'>
+      Created invite link:{' '}
+      <a href={inviteUrl}>
+        <code>{inviteUrl}</code>
+      </a>{' '}
+      for open enrollment.
+    </p>
+  )
 }
 
 function AdminSection(
   { vm }: AdminProps,
-  { bindEffect, events }: ComponentContext<AdminEvents>,
+  { bindEffect, bindImmediateEffect, events }: ComponentContext<AdminEvents>,
 ) {
+  bindImmediateEffect(events.createEmailInvite, (event) => {
+    event.preventDefault()
+    vm.createEmailInvite()
+  })
+  bindImmediateEffect(events.createOpenInvite, () => vm.createOpenInvite())
+  bindImmediateEffect(events.emailChanged, (event) => {
+    const email = (event.target as HTMLInputElement).value
+    vm.emailChanged(email)
+  })
   bindEffect(events.recount, () => vm.recount())
+  const inviteView = vm.invite.pipe(
+    map((invite) => {
+      if (!invite) {
+        return Empty
+      }
+      return () => <InviteView invite={invite} />
+    }),
+  )
   return (
     <section class='section'>
       <h1 class='title'>
@@ -31,6 +82,41 @@ function AdminSection(
         </span>{' '}
         Administration
       </h1>
+      <div childrenBind={inviteView} childrenBindMode='replace' />
+      <p class='block'>
+        An open invitation allows any email to be registered while it is valid
+        (roughly 48 hours).
+      </p>
+      <button
+        type='button'
+        class='block button is-primary'
+        events={{ click: events.createOpenInvite }}
+      >
+        Create New Open Invite
+      </button>
+      <p class='block'>
+        An invite can be created for only a specific email address to register.
+      </p>
+      <form class='block' events={{ submit: events.createEmailInvite }}>
+        <div class='field'>
+          <label class='label'>Email</label>
+          <div class='control'>
+            <input
+              class='input'
+              type='email'
+              placeholder='Enter email address'
+              events={{ change: events.emailChanged }}
+            />
+          </div>
+        </div>
+        <div class='field'>
+          <div class='control'>
+            <button type='submit' class='button is-primary'>
+              Create New Email Invite
+            </button>
+          </div>
+        </div>
+      </form>
       <p class='block'>
         When changes are made to the ballot (a new book is added, an existing
         book was moved to "Held", things like that) the ballots will all
