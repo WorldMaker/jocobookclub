7335630eae9c6e2a0a42f7c6e7bd6a3cd2d2f691
Max Battcher <me@worldmaker.net>
2026-04-30 15:53:57 -0400

    Support overriding the API Url (via localStorage)

--- src/bf/client.ts
+++ src/bf/client.ts
@@ -1,4 +1,10 @@
 import type apiApp from '@worldmaker/jocobookclub-api'
 import { hc } from 'hono/client'
 
-export const apiClient = hc<typeof apiApp>('https://api.jocobook.club')
+const overrideApiUrl = localStorage.getItem('override/api-url')
+const apiUrl = overrideApiUrl ?? 'https://api.jocobook.club'
+if (overrideApiUrl) {
+  console.warn(`Using overridden API URL: ${overrideApiUrl}`)
+}
+
+export const apiClient = hc<typeof apiApp>(apiUrl)
