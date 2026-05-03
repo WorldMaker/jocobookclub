82bcc865de1b2c7e9fec19df7fdb0a393de09c9b
Max Battcher <me@worldmaker.net>
2026-05-03 17:27:01 -0400

    Temporarily shift Production API URLs while waiting for DNS
    Enough complaints that the site isn't working while waiting on Deno to verify the updated DNS records which themselves are updated enough for outage complaints.
    
    This is the "Current" Deno Deploy Production URL.
    
    I expect to switch it back once DNS and Deno Deploy's validation of DNS clear up.

--- scripts/api/client.ts
+++ scripts/api/client.ts
@@ -3,6 +3,6 @@ import type apiApp from '@worldmaker/jocobookclub-api'
 import { hc } from 'npm:hono@^4.6.16/client'
 
 const overrideApiUrl = Deno.env.get('CLUB_API_URL')
-const apiUrl = overrideApiUrl ?? 'https://api.jocobook.club'
+const apiUrl = overrideApiUrl ?? 'https://jocobookclub-api.worldmaker.deno.net/'
 
 export const apiClient = hc<typeof apiApp>(apiUrl)
