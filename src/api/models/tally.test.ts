4cf7777e2feba93de2d6315dc4f82194db87b888
Max Battcher <me@worldmaker.net>
2026-05-03 18:11:34 -0400

    Test a couple bits of tally calculation

---
+++ src/api/models/tally.test.ts
@@ -0,0 +1,41 @@
+import { expect } from '@std/expect'
+import { describe, it } from '@std/testing/bdd'
+import type { Ballot } from './ballot.ts'
+import { getTallyFromBallot, zeroTally } from './tally.ts'
+import type { Preferred } from './preferred.ts'
+
+describe('tally', () => {
+  const eligibleBooks = ['A', 'B', 'C', 'D', 'E']
+  const emptyPreferred: Preferred = { multiplier: 1, userIds: new Set<string>() }
+
+  it('should return a zero tally for an inactive ballot', () => {
+    const ballot: Ballot = {
+      active: false,
+      books: {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+      },
+      updated: new Date(),
+      userId: 'user1',
+    }
+    const tally = getTallyFromBallot(eligibleBooks, ballot, emptyPreferred)
+    const zero = zeroTally(eligibleBooks)
+    expect(tally).toMatchObject(zero)
+  })
+
+  it('should calculate supports from a simple, extreme ballot', () => {
+    const ballot: Ballot = {
+      active: true,
+      books: {
+        D: 5,
+      },
+      updated: new Date(),
+      userId: 'user1',
+    }
+    const tally = getTallyFromBallot(eligibleBooks, ballot, emptyPreferred)
+    expect(tally.supports).toEqual([0, 0, 0, 1, 0])
+  })
+})
